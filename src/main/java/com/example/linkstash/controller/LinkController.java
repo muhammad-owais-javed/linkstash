2da42db4d2193eba7501c5a1e0cc56764bde5174
Owais <50521519+muhammad-owais-javed@users.noreply.github.com>
2026-02-23 07:05:03 +0200

    Add LinkRepository in controller

--- src/main/java/com/example/linkstash/controller/LinkController.java
+++ src/main/java/com/example/linkstash/controller/LinkController.java
@@ -3,6 +3,7 @@ package com.example.linkstash.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.linkstash.model.Link;
+import com.example.linkstash.repository.LinkRepository;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,19 +12,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class LinkController {
 
+
+    private final LinkRepository linkRepository;
+
+    public LinkController(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, LinkStash!";
     }
 
     @PostMapping("/links")
-    public String createLinks(@RequestBody Link newLink) {
+    public Link createLinks(@RequestBody Link newLink) {
         
+        Link savedLink = linkRepository.save(newLink);
+        System.out.println("Saved link: " + savedLink);
+
         System.out.println("Receieved link data: " + newLink);
         System.out.println("URL:" + newLink.getUrl());    
         System.out.println("Title:" + newLink.getTitle());
     
-        return "Link created successfully!";
+        return savedLink;
     }
     
 }
