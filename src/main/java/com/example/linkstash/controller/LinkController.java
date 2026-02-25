1dd69ecf7248a94640cce1831f5cc1fc68f2f2cf
Owais <50521519+muhammad-owais-javed@users.noreply.github.com>
2026-02-25 12:33:40 +0200

    Add getLinkById method

--- src/main/java/com/example/linkstash/controller/LinkController.java
+++ src/main/java/com/example/linkstash/controller/LinkController.java
@@ -6,8 +6,11 @@ import com.example.linkstash.model.Link;
 import com.example.linkstash.repository.LinkRepository;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,11 +21,13 @@ public class LinkController {
     private final LinkRepository linkRepository;
 
     public LinkController(LinkRepository linkRepository) {
+        
         this.linkRepository = linkRepository;
     }
 
     @GetMapping("/hello")
     public String sayHello() {
+        
         return "Hello, LinkStash!";
     }
 
@@ -30,8 +35,8 @@ public class LinkController {
     public Link createLinks(@RequestBody Link newLink) {
         
         Link savedLink = linkRepository.save(newLink);
+       
         System.out.println("Saved link: " + savedLink);
-
         //System.out.println("Receieved link data: " + newLink);
         //System.out.println("URL:" + newLink.getUrl());    
         //System.out.println("Title:" + newLink.getTitle());
@@ -41,10 +46,26 @@ public class LinkController {
 
     @GetMapping ("/links")
     public List<Link> getAllLinks() {
+
         List<Link> allLinks = linkRepository.findAll();
+        
         System.out.println("Retrieved Links: " + allLinks);
     
         return allLinks;
+    }
+
+    @GetMapping("/links/{id}")
+    public ResponseEntity<Link> getLinkById(@PathVariable Long id) {
+       
+        //Link link = linkRepository.findById(id);
+        Optional<Link> foundLink = linkRepository.findById(id);
+
+        if (foundLink.isPresent()) {
+            return ResponseEntity.ok(foundLink.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
     
 }
